878cc727ee6f53066ebb083c4f4bff7a51c9aa7d
Rob Noyes <robertjnoyes1@gmail.com>
2022-02-01 16:08:59 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    case '/contact-me':
+      path += 'contact-me.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log('Server running');
+});
